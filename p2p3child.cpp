296125a87d1a6c281a0944b83eabf2a63fb396b8
A5tral1s <matthew.sanii1@gmail.com>
2025-11-05 12:53:21 -0500

    Update p2p3child.cpp

--- p2p3child.cpp
+++ p2p3child.cpp
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #define KILLSWITCH -500
 
 int main(){
   int p1counter = 0;
   while(1) {
       printf("Child Process (PID: %d), Counter: %d\n", getpid(), p1counter);
-      p1counter--;
       if(p1counter % 3 == 0){
         printf("%d is a multiple of 3.\n", p1counter);
       }
@@ -14,5 +17,6 @@ int main(){
       if(p1counter < KILLSWITCH){
           kill(getpid(), SIGKILL);
       }
+      p1counter--;
   }
 }
