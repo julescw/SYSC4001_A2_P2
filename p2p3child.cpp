aef9f5ec704022a1840885c11d9455590041afba
A5tral1s <matthew.sanii1@gmail.com>
2025-11-05 12:46:19 -0500

    Update p2p3child.cpp

--- p2p3child.cpp
+++ p2p3child.cpp
@@ -5,9 +5,10 @@
 int main(){
   int p1counter = 0;
   while(1) {
-      printf("Child Process (PID: %d), Counter: %d\n", getpid(), p1counter++);
-      if(p2counter % 3 == 0){
-        printf("%d is a multiple of 3.\n", p2counter);
+      printf("Child Process (PID: %d), Counter: %d\n", getpid(), p1counter);
+      p1counter--;
+      if(p1counter % 3 == 0){
+        printf("%d is a multiple of 3.\n", p1counter);
       }
       sleep(1); 
       if(p1counter < KILLSWITCH){
