5b71e4a4cfd35dee9911d1a05900aca021b42227
Julia Wong <jlcharwong@gmail.com>
2025-11-04 20:21:48 -0500

    Create part2problem2.cpp

---
+++ part2problem2.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#define KILLSWITCH 100
+
+/**
+ *
+ * @file part2problem2.cpp
+ * @author Matthew Sanii
+ * @author Jules Wong
+ *
+ */
+
+int main() {
+  
+    pid_t pid;
+    pid = fork();
+
+    if (pid == 0) {
+        execl("./child", "child", NULL);
+        perror("exec failed"); // Only executes if exec fails
+        exit(1);
+      
+    } else if (pid > 0) {
+        int p2counter = 0;
+        while(1) {
+            printf("Parent Process (PID: %d), Counter: %d\n", getpid(), p2counter--);
+            if(p2counter % 3 == 0){
+              printf("%d is a multiple of 3.\n", p2counter);
+            }
+            sleep(1); 
+            if(p2counter == KILLSWITCH){
+                kill(getpid(), SIGKILL);
+            }
+        }
+    }
+}
