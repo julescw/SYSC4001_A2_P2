1abfdd3ed75473fa49b5b9a3929e3cbb92a67c67
A5tral1s <matthew.sanii1@gmail.com>
2025-11-05 12:43:16 -0500

    Update part2problem2.cpp

--- part2problem2.cpp
+++ part2problem2.cpp
@@ -18,17 +18,18 @@ int main() {
     pid = fork();
 
     if (pid == 0) {
-        execl("./p2p2child", "p2p2child", NULL);
+        execlp("./p2p2child", "./p2p2child", (char *)NULL);
         perror("exec failed");
         exit(1);
       
     } else if (pid > 0) {
         int p2counter = 0;
         while(1) {
-            printf("Parent Process (PID: %d), Counter: %d\n", getpid(), p2counter--);
+            printf("Parent Process (PID: %d), Counter: %d\n", getpid(), p2counter);
             if(p2counter % 3 == 0){
               printf("%d is a multiple of 3.\n", p2counter);
             }
+            p2counter++;
             sleep(1); 
             if(p2counter == 0-KILLSWITCH){
                 kill(getpid(), SIGKILL);
